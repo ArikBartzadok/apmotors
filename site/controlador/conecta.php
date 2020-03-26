87291a71f0f5c00f7d8c1f5cb5e7da8af258e012
Diogo Ferreira <45860030+ArikBartzadok@users.noreply.github.com>
2020-03-25 23:12:31 -0300

    Update conecta.php

--- site/controlador/conecta.php
+++ site/controlador/conecta.php
@@ -1,8 +1,8 @@
 <?php
-$servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$banco = "apmotors";
+$servidor = "ec2-34-200-101-236.compute-1.amazonaws.com";
+$usuario = "abakvxfeiruzzv";
+$senha = "f9ae6959cd0a77b0014bb807f7b6aef81b0790a2f5ca3271bfe2e5594a1c289b";
+$banco = "dbuaoc1kif77ut";
 
 $conecta = mysqli_connect($servidor, $usuario, $senha, $banco);
 if(!$conecta) {
