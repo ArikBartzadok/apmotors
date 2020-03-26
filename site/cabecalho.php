fc2486c8411792c72eebdfc71776fa10145af853
Diogo Ferreira <45860030+ArikBartzadok@users.noreply.github.com>
2020-03-25 23:37:04 -0300

    Update cabecalho.php

--- site/cabecalho.php
+++ site/cabecalho.php
@@ -1,5 +1,5 @@
 <div class="wallpaper">
-    <img src="http://localhost/apmotors/site/imagens/wall_1.jpg" class="img-fluid">
+    <img src="https://testediogo.herokuapp.com//site/imagens/wall_1.jpg" class="img-fluid">
     <div class="titulo">
         <center>
         <h1 class="display-4">APMotors</h1>
